2a6886d5705a20eafe5e6ac42f7ab39534348568
AbdurRahaman Shah <a.rahmanshah@gmail.com>
2019-03-10 15:45:24 +0300

    add two strings

---
+++ Interview Preparation Kit/Dictionaries and Hashmaps/Two Strings.js
@@ -0,0 +1,12 @@
+function twoStrings(s1, s2) {
+  var matchFound = "NO";
+
+  for (var letter of s1) {
+    if (s2.includes(letter)) {
+      matchFound = "YES";
+      break;
+    }
+  }
+
+  return matchFound;
+}
