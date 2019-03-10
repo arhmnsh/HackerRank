4913f9a99f55efc68d74a9f0ccf4b8b9db0c4d33
AbdurRahaman Shah <a.rahmanshah@gmail.com>
2019-03-10 15:47:08 +0300

    add sherlock and anagrams

---
+++ Interview Preparation Kit/Dictionaries and Hashmaps/Sherlock and Anagrams.js
@@ -0,0 +1,29 @@
+//https://www.hackerrank.com/challenges/sherlock-and-anagrams
+function sherlockAndAnagrams(s) {
+  let count = 0;
+
+  for (let i = 1; i < s.length; i++) {
+    let found = {};
+
+    for (let j = 0; j + i <= s.length; j++) {
+      console.log("i", i);
+      console.log("j", j);
+      console.log("i+j", i + j);
+      let substr = s.substr(j, i);
+      substr = substr
+        .split("")
+        .sort()
+        .join("");
+      console.log(substr);
+
+      if (found[substr]) {
+        count += found[substr];
+        found[substr]++;
+      } else {
+        found[substr] = 1;
+      }
+    }
+  }
+
+  return count;
+}
