740a3d8d724a148cf05b456b0548969e9dc3efff
AbdurRahaman Shah <a.rahmanshah@gmail.com>
2019-03-11 15:37:38 +0300

    add birthday Cake Candles

---
+++ Algorithms/Warmup/Birthday Cake Candles.js
@@ -0,0 +1,12 @@
+//
+function birthdayCakeCandles(ar) {
+    let maxVal = Math.max.apply(null, ar);
+
+    var maxCount = 0;
+    ar.forEach(val => {
+    if (val == maxVal) maxCount++;
+    });
+
+    console.log(maxCount);
+    return maxCount;
+}
