326b5cdcadd699309b7c04e38db722f2a91c3e37
AbdurRahaman Shah <a.rahmanshah@gmail.com>
2019-03-16 08:36:06 +0300

    simpler solution...

--- Algorithms/Strings/Two Strings.js
+++ Algorithms/Strings/Two Strings.js
@@ -2,14 +2,15 @@
 function twoStrings(s1, s2) {
   // console.log('s1', s1);
   // console.log('s2', s2);
-  var s1A = s1.split("");
-  var s2A = s2.split("");
 
   var matchFound = "NO";
-  for (var i = 0; i < s1A.length; i++) {
-    if (s2A.includes(s1A[i])) {
+
+  for (var letter of s1) {
+    if (s2.includes(letter)) {
       matchFound = "YES";
       break;
     }
   }
+
+  return matchFound;
 }
