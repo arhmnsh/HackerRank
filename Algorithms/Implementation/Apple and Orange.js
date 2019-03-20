f40eb44db4258208d312271fc516c33ec8aa2764
AbdurRahaman Shah <a.rahmanshah@gmail.com>
2019-03-20 08:54:24 +0300

    add count apples and oranges

---
+++ Algorithms/Implementation/Apple and Orange.js
@@ -0,0 +1,19 @@
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+  let appleDistances = apples.map(apple => a + apple);
+  let orangeDistances = oranges.map(orange => b + orange);
+
+  let applesOnHouse = appleDistances.reduce(
+    (acc, val) => (val >= s && val <= t ? acc + 1 : acc),
+    0
+  );
+
+  let orangesOnHouse = orangeDistances.reduce(
+    (acc, val) => (val >= s && val <= t ? acc + 1 : acc),
+    0
+  );
+
+  console.log(applesOnHouse);
+  console.log(orangesOnHouse);
+}
+
+countApplesAndOranges(7, 10, 4, 12, [2, 3, -4], [3, -2, -4]);
